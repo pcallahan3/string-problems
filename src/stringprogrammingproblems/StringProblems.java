186d193c4374d928648699c39e8b0f25f0b7f169
Patrick <pcallahan3@mail.greenriver.edu>
2019-12-08 01:30:54 -0800

    Wrote problem statement and psuedocode for reverseAString() function

--- src/stringprogrammingproblems/StringProblems.java
+++ src/stringprogrammingproblems/StringProblems.java
@@ -4,10 +4,24 @@ public class StringProblems {
 
 	public static void main(String[] args) {
 	
+	
+		 	    
 
 	}
 	
+	/* Problem statement: Reverse a string without using the built in reverse() function. 
+	 * Psuedocode for reverseAString() function: 
+	 *   1. Create a function that accepts a string as parameter and returns the reversed string.
+	 *   2. Declare and initialize a character array named stringChars and convert the string to an array of characters.
+	 *   3. Using a for loop set the index to the first index of the array of characters until the index is greater than or equal to 0,
+	 *      and decrement the index.
+	 *   4. Print the character arrays index.
+	 *   5. Return the string.
+	 *      
+	 * 
+	 */
 	
+
 	
 
 }
